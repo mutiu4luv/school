253613201e60b01fb7f8645f56f9b8b424090d7e
mutiu4luv <chidiemmamadu@gmail.com>
2023-02-08 07:49:20 +0100

    third comit

--- src/component/course-latest/Course.jsx
+++ src/component/course-latest/Course.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../course-latest/course.css"
 import course1 from '../../images/course1.jpg'
 import course2 from '../../images/course2.jpg'
@@ -7,10 +7,55 @@ import course4 from '../../images/course4.jpg'
 
 import { AiFillStar } from "react-icons/ai";
 import { Link } from 'react-router-dom';
-import { Carousel } from 'react-bootstrap';
+import {  Col } from 'react-bootstrap';
+import axios from 'axios'
+import Carousel from 'react-material-ui-carousel'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import _ from "lodash"
 
-const Course = () => {
-  return (
+    const Course = () => {
+        const pageSize =10
+    const [news, setNews] = useState([]);
+    const [posts, setPosts] = useState()
+    const [paginatedPosts, setPaginatedPosts] = useState()
+    const [currentPage, setCurrentPage] = useState(1)
+
+    
+    useEffect(() => {
+      const fetchNews = async () => {
+        const { data } = await axios.get("http://jsonplaceholder.typicode.com/posts");
+  
+        console.log(data)
+        setNews(data);
+        setPosts(data)
+        setPaginatedPosts(_(data).slice(0).take(pageSize).value())
+        
+      };
+  
+      fetchNews();
+    }, []);
+
+    const handleChange = (e, p) =>{
+        console.log(e, p)
+        // setPage(p)
+    }
+
+const pageCount = posts? Math.ceil(posts.length/pageSize) :0;
+if (pageCount ===1) return null
+const pages = _.range(1, pageCount+1)
+  
+
+const pagination=(pageNo)=>{
+    setCurrentPage(pageNo)
+    const startIndex = (pageNo -1)* pageSize
+    const paginatedPost = _(posts).slice(startIndex).take(pageSize).value()
+    setPaginatedPosts(paginatedPost)
+}
+
+
+
+return (
     <div className='latest container'>
          <div className='content'>
 <h6>  <span>L</span>atest Courses </h6> 
@@ -20,18 +65,27 @@ const Course = () => {
 
 </div>
 
-<Carousel>
-<div className='row'>
 
-    <div className='col-sm-3'>
-    {/* <Carousel.Item> */}
+
+<div className='grid-item '>{
+    !paginatedPosts ? ("No Data Found") : (
+  
+
+<div className='row ' >
+{paginatedPosts?.map((news) => (
+          <div key={news._id} sm={12} md={6} lg={4} xl={3}>
+            <news news={news} />
+ 
+
+    <div className='col-sm-3 '   >
+
+    
         <div className='box'>
             <img src={course1} className="img-fluid" alt='trxyt' />
            
 
             <div className='boxContent'>
-            {/* <Carousel.Caption> */}
-            <h6>Art</h6>
+            <h6>{news.title}</h6>
             <h5>20 principles</h5>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolorem!</p>
             <AiFillStar />
@@ -42,87 +96,49 @@ const Course = () => {
             <AiFillStar />
                 <br /> 
             <Link>Buy Now</Link>      
-            {/* </Carousel.Caption> */}
             </div> 
             
 
         </div>
-        {/* </Carousel.Item> */}
+     
+
     </div>
-  
+
+
+
+
 
     
-    
-
-     <div className='col-sm-3'>
-        <div className='box'>
-            <img src={course2} className="img-fluid" alt='trxyt' />
-            <div className='boxContent'>
-            <h6>science</h6>
-            <h5>16 lecturers</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolorem!</p>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-                <br /> 
-            <Link>Buy Now</Link>      
-            </div> 
-        </div>
-    </div>
-    <div className='col-sm-3'>
-        <div className='box'>
-            <img src={course3} className="img-fluid" alt='trxyt' />
-            <div className='boxContent'>
-            <h6>building</h6>
-            <h5>27 lecturers</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolorem!</p>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-                <br /> 
-            <Link>Buy Now</Link>      
-            </div> 
-        </div>
-    </div>
-    <div className='col-sm-3'>
-        <div className='box'>
-            <img src={course4} className="img-fluid" alt='trxyt' />
-            <div className='boxContent'>
-            <h6>optometery</h6>
-            <h5>20 lecturers</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolorem!</p>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-                <br /> 
-            <Link>Buy Now</Link>      
-            </div> 
-        </div>
-    </div>
-    
-   
-
-
-
-
-
-
+             
 </div>
-{/* </Carousel.Item> */}
+    ))}
 
-</Carousel>
+</div>  )}
 
+
+
+
+</div>  
+<nav className='d-flex justify-content-center'>
+    <ul className='pagination'>
+{
+    pages.map((page)=>(
+        <li className={
+            page === currentPage ? "page-item active" : "page-item"
+        }>
+            <p className='page-link' onClick={()=>pagination(page)}>
+      {page}
+            </p>
+            </li>
+
+    ))
+}
+
+    
+    </ul>
+</nav>
     </div>
- 
+
 
   )
 }
